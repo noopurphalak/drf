00ff3879fd0557fab452a988006b3567865f7987
Noopur Phalak <noopur@coverfoxmail.com>
2018-07-25 21:38:39 +0530

    Class Based Views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,14 +3,60 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
+# ######################### VIEWS USING GENERIC VIEWS #########################
+class SnippetGenericList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetGenericDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+# ######################### VIEWS USING MIXINS ################################
+class SnippetListMixinView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetailMixinView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
 # ######################### VIEWS USING APIVIEW ###############################
-
-
 class SnippetList(APIView):
     """
     List all code snippets, or create a new snippet.
@@ -59,8 +105,6 @@ class SnippetDetail(APIView):
 
 
 # ######################### FUNCTION BASED VIEWS #################################
-
-
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
     """
